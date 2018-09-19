6e47555e98b524218255566f4e6ffbf1fc613e56
Alberto Gil <agildav@gmail.com>
2018-09-18 20:24:09 -0400

    Added CHANGE_SEARCH_FIELD test

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -1,8 +1,16 @@
 import { searchRobots } from "./reducers";
+import { CHANGE_SEARCH_FIELD } from "./constants";
 
 describe("searchRobots", () => {
   test("return the initial state", () => {
     expect.assertions(1);
     expect(searchRobots({}, {})).toEqual({});
+  });
+
+  test("should process CHANGE_SEARCH_FIELD", () => {
+    expect.assertions(1);
+    expect(
+      searchRobots({}, { type: CHANGE_SEARCH_FIELD, payload: "abc" })
+    ).toEqual({ searchfield: "abc" });
   });
 });
